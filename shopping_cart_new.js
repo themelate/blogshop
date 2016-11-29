b5965e9566b29ba4654ea313c63cacb1cb4eed9a
themelate <djiextray@gmail.com>
2016-11-29 10:42:05 +0700

    Update shopping_cart_new.js

--- shopping_cart_new.js
+++ shopping_cart_new.js
@@ -1,3 +1,60 @@
+
+$(document).ready(function(){
+	loadProvinsi('#oriprovince');
+	loadProvinsi('#desprovince');
+	$('#oriprovince').change(function(){
+		$('#oricity').show();
+		var idprovince = $('#oriprovince').val();
+		loadCity(idprovince,'#oricity')
+	});
+	$('#desprovince').change(function(){
+		$('#descity').show();
+		var idprovince = $('#desprovince').val();
+		loadCity(idprovince,'#descity')
+	});
+});
+
+function loadProvinsi(id){
+	$('#oricity').hide();
+	$('#descity').hide();
+	$(id).html('loading...');
+	$.ajax({
+		url:'http://files.themelate.com/process.php?act=showprovince',
+		dataType:'json',
+		success:function(response){
+			$(id).html('');
+			province = '';
+				$.each(response['rajaongkir']['results'], function(i,n){
+					province = '<option value="'+n['province_id']+'">'+n['province']+'</option>';
+					province = province + '';
+					$(id).append(province);
+				});
+		},
+		error:function(){
+			$(id).html('ERROR');
+		}
+	});
+}
+function loadCity(idprovince,id){
+	$.ajax({
+		url:'http://files.themelate.com/process.php?act=showcity',
+		dataType:'json',
+		data:{province:idprovince},
+		success:function(response){
+			$(id).html('');
+			city = '';
+				$.each(response['rajaongkir']['results'], function(i,n){
+					city = '<option value="'+n['city_id']+'">'+n['city_name']+'</option>';
+					city = city + '';
+					$(id).append(city);
+				});
+		},
+		error:function(){
+			$(id).html('ERROR');
+		}
+	});
+}
+
 function scs()
 {
 	simpleCart.empty();
