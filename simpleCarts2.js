c3c83b0de0fa610bdf54ccf750e01cc86f0acbdc
themelate <djiextray@gmail.com>
2016-11-30 09:44:55 +0700

    Update simpleCarts2.js

--- simpleCarts2.js
+++ simpleCarts2.js
@@ -39,7 +39,7 @@ function cart( email , matauang) {
 	this.totalPrice		= 0.00;
 	this.totalWeight	= 0.0;
 	this.totalShip		= ongkir;
-	this.totalOrder 	= ongkir;
+	this.totalOrder 	= 100.00;
 	this.items 			= new Array();
 	this.userEmail 		= email;
 	// order of columns, you change the order here or by accessing the value in your html
